bd4ea35b8456a6420e2b7b57b1f0d9a7dabf5d24
sho1999 <management14sho@gmail.com>
2021-11-15 15:31:30 +0900

    学生データ詳細機能
    ・一覧表示のコンテンツを少なく

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:edit, :show]
 
   def index
     @students = Student.all
@@ -12,8 +13,10 @@ class StudentsController < ApplicationController
     Student.create(student_params)
   end
 
+  def show
+  end
+
   def edit
-    @student =  Student.find(params[:id])
   end
 
   def update
@@ -30,6 +33,10 @@ class StudentsController < ApplicationController
   def student_params
     params.require(:student).permit(:name, :college_name, :gakubu_name, :gakka_name, :one_word, :exp_occ, :circle_name, :qualification_name,
                                     :intern_history_id, :graduation_id, :prefecture_id, :industry_id, :gender_id, :grade_id, :sales_amount)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'students#index'
-  resources :students, only: [:index, :new, :create, :destroy, :edit, :update]
+  resources :students
 end

d0984e67f4ceb35449bd1bf1e9772f18d459a01d
sho1999 <management14sho@gmail.com>
2021-11-15 15:23:44 +0900

    学生データの登録機能実装

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,32 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    Student.create(student_params)
+  end
+
+  def edit
+    @student =  Student.find(params[:id])
+  end
+
+  def update
+    student = Student.find(params[:id])
+    student.update(student_params)
+  end
+
+  def destroy
+    student = Student.find(params[:id])
+    student.destroy
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :college_name, :gakubu_name, :gakka_name, :one_word, :exp_occ, :circle_name, :qualification_name,
+                                    :intern_history_id, :graduation_id, :prefecture_id, :industry_id, :gender_id, :grade_id, :sales_amount)
+  end
+
 end

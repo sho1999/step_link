265d3fb00d3b054c6efce230d279a86779baead3
sho1999 <management14sho@gmail.com>
2021-12-06 20:29:09 +0900

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  # devise_for :corp_users, controllers: {
-  #   sessions: 'corp_user/sessions',
-  #   passwords: 'corp_user/passwords',
-  #   registrations: 'corp_user/registrations'
-  # }
-  root to: 'students#index'
-  get 'students/search'
-
-  resources :students
   devise_for :student_users
   resources :student_users, only: :show
-
   devise_for :corp_users
+  resources :students
   resources :corp_users, only: :show
+
+  root to: 'students#index'
+  get 'students/search'
 end

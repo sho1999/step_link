25a2fd8803ea9076b2a0bfdab0bc1d3e91663dba
sho1999 <management14sho@gmail.com>
2021-12-06 16:55:25 +0900

    企業情報の変更機能
    ・プロフィ変更機能
    ・ログアウト機能
    ・アカウント削除機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :location, :lcorp_name, :staff_msg, :industry_id, :feature_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :location, :corp_name, :lcorp_name, :staff_msg, :industry_id, :feature_id])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :location, :corp_name, :lcorp_name, :staff_msg, :industry_id, :feature_id])
   end
-
 end
